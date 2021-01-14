83bf68c7548012e25b11edeb5798fc6978affdc2
ANF-Studios <theanfstudios@gmail.com>
2021-01-14 15:57:39 -0500

    Fomratted document

--- examples/get_private_paste_async.rs
+++ examples/get_private_paste_async.rs
@@ -7,7 +7,11 @@ type Result<T, E = Error> = std::result::Result<T, E>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let paste: PasteObject = paste::get_private_paste_async("pasteID", "Your PasteMyst Token. Get it from: https://paste.myst.rs/user/settings").await?;
+    let paste: PasteObject = paste::get_private_paste_async(
+        "pasteID",
+        "Your PasteMyst Token. Get it from: https://paste.myst.rs/user/settings",
+    )
+    .await?;
     println!("{}", paste.isPrivate);
     Ok(())
 }
