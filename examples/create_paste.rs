c9266ce1d702316e68a6872e15982d751a89c70b
ANF-Studios <theanfstudios@gmail.com>
2021-01-13 23:28:08 -0500

    Added create_paste example

---
+++ examples/create_paste.rs
@@ -0,0 +1,30 @@
+// create_paste
+use pastemyst_rs::paste::*;
+
+fn main() -> Result<(), reqwest::Error> /*PasteResult<()>*/ {
+    let pasties: Vec<PastyObject> = vec![
+        PastyObject {
+            _id: None,
+            language: Some(String::from("autodetect")),
+            title: Some(String::from("A pasty title")),
+            code: Some(String::from("fn main() { println!(\"Hello World!\"); }")),
+        },
+        PastyObject {
+            _id: None,
+            title: Some(String::from("Another pasty title")),
+            language: Some(String::from("autodetect")),
+            code: Some(String::from("#include \"stdio.h\"\n\nint main() {\n\tprintf(\"Hello World!\");\n}")),
+        },
+    ];
+    let data: CreateObject = CreateObject {
+        title: String::from("This is a paste title"),
+        expiresIn: String::from("1d"),
+        isPrivate: false,
+        isPublic: false,
+        tags: String::from(""),
+        pasties: pasties,
+    };
+    let paste = create_paste(data)?;
+    println!("{:#?}", paste.text()?);
+    Ok(())
+}
