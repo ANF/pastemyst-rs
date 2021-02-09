11c47a7221f3d0084b376540f0f31e9bf7c9029e
ANF-Studios <theanfstudios@gmail.com>
2021-02-09 17:00:49 -0500

    Added macro documentation

--- src/lib.rs
+++ src/lib.rs
@@ -253,6 +253,23 @@ pub mod paste {
     /// }
     /// ```
     pub type PasteResult<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
+
+    /// This macro can assist prevention of
+    /// converting an str to a String and
+    /// calling that method each time.
+    ///
+    /// It can be used for other purposes
+    /// if you wish to, but it's designed
+    /// to work with any struct field that
+    /// takes in a String type. Not necessarily
+    /// recommened to be used however you can
+    /// if you wish to but it's not compulsory.
+    ///
+    /// You can also use other macros like
+    /// methods like `String::from` or even
+    /// `to_string()` if you wish to.
+    ///
+    /// This method calls the `String::from` method.
     #[macro_export]
     macro_rules! str {
         {$value:expr} => (String::from($value));
